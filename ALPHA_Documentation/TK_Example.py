365d6450ba142fb04349d5b2671f7b48fde42aa4
Jeff Cherry <cherry.jeff@epa.gov>
2019-09-30 13:06:13 -0400

    Update #4

--- ALPHA_Documentation/TK_Example.py
+++ ALPHA_Documentation/TK_Example.py
@@ -4,10 +4,52 @@
 
    Handy set of functions"""
 
+# Import the modules
+import sys
+import random
+
+from pip._vendor.distlib.compat import raw_input
+
+ans = True
+
+while ans:
+    question = raw_input("Ask the magic 8 ball a question: (press enter to quit) ")
+
+    answers = random.randint(1, 8)
+
+    if question == "":
+        sys.exit()
+
+    elif answers == 1:
+        print("It is certain")
+
+    elif answers == 2:
+        print("Outlook good")
+
+    elif answers == 3:
+        print("You may rely on it")
+
+    elif answers == 4:
+        print("Ask again later")
+
+    elif answers == 5:
+        print("Concentrate and ask again")
+
+    elif answers == 6:
+        print("Reply hazy, try again")
+
+    elif answers == 7:
+        print("My reply is no")
+
+    elif answers == 8:
+        print("My sources say no")
+
+"""Handy set of functions"""
 
 import os
 import shutil
 import sys
+
 
 
 print('Loading %s...' % __name__)
