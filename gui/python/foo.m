2be398f4f0833345442f098514501e5e08e23c70
kevinanewman <newman.kevin@epa.gov>
2021-01-07 17:28:10 -0500

    doc updates

--- gui/python/foo.m
+++ gui/python/foo.m
@@ -1,6 +1,10 @@
 % this is a matlab file
 
 function [bar] = foo(a,b,c)
-% FOO = a + b + c
+% a fun function
+%
+% :param a1: the first input
+% :param a2: another input
+% :returns: ``[o1, o2, o3]`` some outputs
 
 return a + b + c
