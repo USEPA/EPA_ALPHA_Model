8417cc8973f558c879963ea4b642ce1c4b4acce0
kevinanewman <newman.kevin@epa.gov>
2021-01-07 14:54:23 -0500

    doc updates

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(path, '..', 'gui', 'python'))
+sys.path.insert(0, os.path.join(path, '', 'gui', 'python'))
 
 # -- Project information -----------------------------------------------------
 
