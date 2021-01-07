59993317483f13354526ce4dea51d7b4430b285d
kevinanewman <newman.kevin@epa.gov>
2021-01-07 15:50:36 -0500

    doc updates

--- ALPHA_Documentation/conf.py
+++ ALPHA_Documentation/conf.py
@@ -47,7 +47,7 @@ numfig = True
 # The theme to use for HTML and HTML Help pages.   See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 
 # EPA Palette
 html_theme_options = {
