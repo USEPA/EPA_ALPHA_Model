8e347093875673bd610ae6247fbce45a66b52d7a
kevinanewman <newman.kevin@epa.gov>
2021-01-11 12:29:30 -0500

    doc updates

--- ALPHA_Documentation/conf.py
+++ ALPHA_Documentation/conf.py
@@ -41,7 +41,7 @@ extensions = ['sphinxcontrib.matlab', 'sphinx.ext.autodoc']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'model', 'archive', '_sequester', 'Thumbs.db', '.DS_Store']
 
 numfig = True
 
