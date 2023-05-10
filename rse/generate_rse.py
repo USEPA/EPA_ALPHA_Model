57a62d19bf1718f88b0d9021cc92e0f3364a2399
jcherr01 <cherry.jeff@epa.gov>
2023-05-10 14:51:40 -0400

    RSE Update

--- rse/generate_rse.py
+++ rse/generate_rse.py
@@ -3,37 +3,42 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import csv
 
-# Load the CSV file using NumPy's genfromtxt function
-data = np.genfromtxt('book1.csv', delimiter=',', skip_header=1)
+# Load the input CSV
+data = np.genfromtxt('ttt_1.csv', delimiter=',', skip_header=1)
 
-# Extract the second column into a separate array
+# Extract the data into arrays
 x1 = data[:, 0]
 x2 = data[:, 1]
 x3 = data[:, 2]
 x4 = data[:, 3]
 y = data[:, 4]
 
-# Create the design matrix
+# Define the RSE parameters
 X = np.column_stack((x1, x2, x3, x4))
-poly = PolynomialFeatures(degree=3)
+poly = PolynomialFeatures(degree=2)
 X_design = poly.fit_transform(X)
 
-# Fit the linear regression model
+# Solve the RSE
 model = LinearRegression()
 model.fit(X_design, y)
 
-# Print the model coefficients
+# Get the model coefficients
 intercept = model.intercept_
 coeff = model.coef_
 
 # Get feature names
 feat_names = poly.get_feature_names_out(['RLHP20', 'RLHP60', 'HP_ETW', 'ETW'])
 
-# Create equation string
+# Generate RSE equation for export
 equation = f"{intercept:.15f} + "
 for i, name in enumerate(feat_names):
     feat_names[i] = feat_names[i].replace(" ", " * ")
     equation = equation + str(coeff[i]) + " * " + feat_names[i] + " + "
 equation = equation[:-3]  # Remove the last " + "
 
-print(equation)
+# print(equation)
+
+# Export equation to file
+output_file = open("equation.txt", "w")
+output_file.write(equation)
+output_file.close()
